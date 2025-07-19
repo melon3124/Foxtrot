2e5caf3b379acc9da38fcc6a75092354e973b273
melon3124 <garceronrico24@gmail.com>
2025-07-20 04:48:18 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -15,9 +15,11 @@ if not st.session_state.auth_ok:
     pw = st.text_input("🔐 Enter password to access Foxtrot CIS", type="password")
     if pw == "C00L$kill$":
         st.session_state.auth_ok = True
+        st.experimental_rerun()  # <--- This is needed to refresh the app
     elif pw:
         st.error("Incorrect password.")
     st.stop()
+
 
 
 # -------------------- CONFIG --------------------
