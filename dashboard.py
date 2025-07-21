2b6a44eac55dbfbe27c01a844a3355a862308a83
melon3124 <garceronrico24@gmail.com>
2025-07-21 20:16:18 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -458,9 +458,6 @@ if st.session_state.mode == "class" and cls:
                                             st.info("No valid new reports to submit.")
                                     except Exception as e:
                                         st.error(f"❌ Error submitting to 'Reports' sheet: {e}")
-    except Exception as e:
-        st.error(f"Conduct tab error: {e}")
-
 
             except Exception as e:
                 st.error(f"Conduct tab error: {e}")
