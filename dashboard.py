f2f714359705174ec7b2f000406c605ff73940a3
melon3124 <garceronrico24@gmail.com>
2025-07-21 23:54:34 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -459,7 +459,7 @@ if st.session_state.mode == "class" and cls:
                                 st.rerun()
                             except Exception as e:
                                 st.error(f"❌ Error submitting to 'REPORTS' sheet: {e}")
-        
-                    except Exception as e:
-                        st.error(f"Conduct tab error: {e}")
+
+            except Exception as e:
+                st.error(f"Conduct tab error: {e}")
 
