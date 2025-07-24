e9f0056c781de7e02bd2ed3feaec44962dcb40eb
melon3124 <garceronrico24@gmail.com>
2025-07-24 08:41:32 +0800

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,10 @@
+import streamlit as st
+
+def authenticate():
+    if "authenticated" not in st.session_state:
+        password = st.text_input("Enter password", type="password")
+        if password == "your_secret":
+            st.session_state["authenticated"] = True
+        else:
+            st.stop()
 
