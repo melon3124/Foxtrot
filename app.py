bd18f99c5bd4f8de25693a5e25c9f29c5d253162
melon3124 <garceronrico24@gmail.com>
2025-08-03 10:03:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -276,8 +276,12 @@ if st.session_state.view == "summary":
                 acad_df[subject] = pd.to_numeric(acad_df[subject], errors='coerce')
                 prof = acad_df[acad_df[subject] >= 7][["NAME", subject]].dropna().sort_values(by=subject, ascending=False)
                 defn = acad_df[acad_df[subject] < 7][["NAME", subject]].dropna().sort_values(by=subject)
-                 else:
+            
+                if not prof.empty:
+                    st.dataframe(prof, use_container_width=True, hide_index=True)
+                else:
                     st.info("No proficient cadets.")
+
 
                     with col2:
                         st.markdown("**🚫 Deficient Cadets**")
